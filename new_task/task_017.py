9b0159bfed54a2d19e5637537e6e1930d7325451
Sergey-2021 <tatarinov.serzhick@yandex.ru>
2022-01-31 18:48:39 +0300

    add decision

--- new_task/task_017.py
+++ new_task/task_017.py
@@ -1,7 +1,26 @@
 # 17.Задать список из N элементов, заполненных числами из [-N, N]. Найти произведение элементов на указанных позициях.
 # Позиции хранятся в файле file.txt в одной строке одно число
 
-print(0.1 + 0.1 + 0.1 == 0.3)
-print(0.3 % 3 * 3)
-print(0.3 * 3)
-print(0.3 * 0.3)
+def f(x):
+    data = open('File.txt', 'w')
+    for i in range(-x, x + 1):
+        data.write(f'{i}\n')
+
+
+def t(a, b):
+    data = open('File.txt', 'r')
+    count = 0
+    for i in data:
+        count += 1
+        if count == a:
+            a = int(i)
+        if count == b:
+            b = int(i)
+    return a * b
+
+
+n = 9
+f(n)
+position_1 = 1
+position_2 = 12
+print(t(position_1, position_2))
