9b1b9685e1df7eff10360021ae29a9a0398b3103
Sergey-2021 <tatarinov.serzhick@yandex.ru>
2022-02-04 06:58:23 +0300

    optimized

--- new_task/task_031.py
+++ new_task/task_031.py
@@ -3,15 +3,14 @@
 def f(x):
     lst = []
     a = x
-    for i in range(2, x//9):
-        if i == i:
+    for i in range(2, x // 2 + 1):
+        if i == 2 or i % 2 != 0:
             while a % i == 0:
-                a = a / i
+                a /= i
                 lst.append(i)
 
     return lst
 
 
-a = 45012324
-
+a = 4234653
 print(f(a))
